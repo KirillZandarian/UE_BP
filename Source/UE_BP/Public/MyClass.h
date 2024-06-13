bfd46aa5666d91f2a357455c8550ca01dbd7d7d0
Кирилл <107216580+KirillZandarian@users.noreply.github.com>
2024-06-13 14:36:21 +0300

    -

---
+++ Source/UE_BP/Public/MyClass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class UE_BP_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+	CHAR Name = 0101;
+};
